8e89b96919bc66cf6fef1d8b949ce4e4c2abd7cb
mathpvx <7547@holbertonstudents.com>
2025-11-07 15:30:21 +0100

    var in print condition

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,15 +1,13 @@
 void main(List<String> args) {
   var nowanint = int.parse(args[0]);
 
-  print(nowanint);
-
   if(nowanint < 0){
-    print("is negative");
+    print("$nowanint is negative");
   }
     if(nowanint == 0){
-    print("is zero");
+    print("$nowanint is zero");
   }
     if(nowanint > 0){
-    print("is positive");
+    print("$nowanint is positive");
   }
 }
