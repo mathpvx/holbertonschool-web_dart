2606d85b0e22a0b6eaff5c21aa0f22cbf57f626f
mathpvx <7547@holbertonstudents.com>
2025-11-07 15:15:36 +0100

    assertion error throw

---
+++ 0x00-hello-dart/5-assertion.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var score = int.parse(args[0]);
+  print(score);
+
+  if (score >= 80) {
+    print("You passed");
+  }
+  
+  assert(score >= 80, "The score must be bigger or equal to 80\n");
+}
