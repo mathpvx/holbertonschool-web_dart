90de9a0a26eec2c03dd4e1604c813ebd004a0593
mathpvx <7547@holbertonstudents.com>
2025-11-07 15:17:59 +0100

    uppercase

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,7 +1,7 @@
 void main(List<String> args) {
   var score = int.parse(args[0]);
   if (score >= 80) {
-    print("You passed");
+    print("You Passed");
   }
   
   assert(score >= 80, "The score must be bigger or equal to 80\n");
