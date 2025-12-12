7cd9f36bcac8cea5505c2c15d18def8cce092e0c
mathpvx <7547@holbertonstudents.com>
2025-12-12 13:55:32 +0100

    get sum

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,27 +1,24 @@
 import 'dart:convert';
 import '4-util.dart';
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
-    // 1) Get user id
     final userData = await fetchUserData();
     final userMap = jsonDecode(userData) as Map<String, dynamic>;
     final id = userMap['id'] as String;
 
-    // 2) Get user orders (JSON list)
     final ordersJson = await fetchUserOrders(id);
     final orders = (jsonDecode(ordersJson) as List).cast<String>();
 
-    // 3) Sum product prices
-    double total = 0.0;
+    num total = 0;
     for (final product in orders) {
       final priceJson = await fetchProductPrice(product);
-      final price = jsonDecode(priceJson); // can be int or double
-      total += (price as num).toDouble();
+      final price = jsonDecode(priceJson) as num; // int or double
+      total += price;
     }
 
     return total;
   } catch (_) {
-    return -1;
+    return -1; // prints -1 (not -1.0)
   }
 }
