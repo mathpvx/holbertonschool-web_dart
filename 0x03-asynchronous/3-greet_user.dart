3543629f090dc5371bbfb41d127057f2dc93ccd6
mathpvx <7547@holbertonstudents.com>
2025-12-12 11:33:06 +0100

    greet use

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    final data = await fetchUserData();
+    final decoded = jsonDecode(data);
+    return 'Hello ${decoded['username']}';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    final hasUser = await checkCredentials();
+    print('There is a user: $hasUser');
+
+    if (hasUser) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
